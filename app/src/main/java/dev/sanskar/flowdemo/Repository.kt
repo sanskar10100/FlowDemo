63183e22a93ef8df225b61e3a0a23e7bf085b5a8
Sanskar Agrawal <sanskar10100@gmail.com>
2022-01-19 19:51:08 +0530

    Add temp exception handler

--- app/src/main/java/dev/sanskar/flowdemo/Repository.kt
+++ app/src/main/java/dev/sanskar/flowdemo/Repository.kt
@@ -1,16 +1,24 @@
 package dev.sanskar.flowdemo
 
+import android.util.Log
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.lang.Exception
+private const val TAG = "Repository"
 
 // Repository
 object Repository {
     val latestResponse: Flow<ApiResponse> = flow {
         while(true) {
-            val response = ApiService.retrofitService.getRecords()
-            emit(response)
-            delay(1000)
+            try {
+                val response = ApiService.retrofitService.getRecords()
+                emit(response)
+                delay(1000)
+            } catch (e: Exception) {
+                // TODO handle through UiState wrapper
+                Log.d(TAG, "An exception occurred: ${e.message}")
+            }
         }
     }
 }
